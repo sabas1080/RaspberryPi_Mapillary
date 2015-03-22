73a17cb9c70b2f687da5b249b1d8e066a0cc1583
Sabas <sabasjimenez@gmail.com>
2015-03-22 13:06:11 -0600

    Modified cam.py

--- cam.py
+++ cam.py
@@ -146,77 +146,77 @@ class Button:
 # UI callbacks -------------------------------------------------------------
 # These are defined before globals because they're referenced by items in
 # the global buttons[] list.
-class GpsPoller(threading.Thread):
-  def __init__(self):
-	threading.Thread.__init__(self)
-	global gpsd #bring it in scope
-	#gpsd = gps(mode=WATCH_ENABLE) #starting the stream of info
-	#gpsd = gps.gps(mode=gps.WATCH_ENABLE)
-	self.current_value = None
-	self.running = True #setting the thread running to true
+#class GpsPoller(threading.Thread):
+#  def __init__(self):
+#	threading.Thread.__init__(self)
+#	global gpsd #bring it in scope
+#	#gpsd = gps(mode=WATCH_ENABLE) #starting the stream of info
+#	#gpsd = gps.gps(mode=gps.WATCH_ENABLE)
+#	self.current_value = None
+#	self.running = True #setting the thread running to true
  
-  def run(self):
-	global gpsd, report
-	gpsd = gps.gps(mode=gps.WATCH_ENABLE)
-	while gpsd.running:
-	  report = gpsd.next() #this will continue to loop and grab EACH set of gpsd info to clear the buffer
+#  def run(self):
+#	global gpsd, report
+#	gpsd = gps.gps(mode=gps.WATCH_ENABLE)
+#	while gpsd.running:
+#	  report = gpsd.next() #this will continue to loop and grab EACH set of gpsd info to clear the buffer
 
 
 def wait():
 	global camera
-	#while True:
-	report = gpsd.next()
-	print report
-	# Wait for position information.
-	if report['class'] == 'TPV':
-		# Set orientation to normal landscape.
-		camera.exif_tags['IFD0.Orientation'] = '1'
+	while True:
+	    report = gpsd.next()
+	    print report
+	    # Wait for position information.
+	    if report['class'] == 'TPV':
+		    # Set orientation to normal landscape.
+		    camera.exif_tags['IFD0.Orientation'] = '1'
  
-		# Set picture date and time to GPS values.
-		now = parser.parse(report.get('time', datetime.now().isoformat()))
-		print now.strftime('%s')
-		camera.exif_tags['EXIF.DateTimeOriginal'] = now.strftime('%Y:%m:%d %H:%M:%S')
+		    # Set picture date and time to GPS values.
+		    now = parser.parse(report.get('time', datetime.now().isoformat()))
+		    print now.strftime('%s')
+		    camera.exif_tags['EXIF.DateTimeOriginal'] = now.strftime('%Y:%m:%d %H:%M:%S')
  
-		# Set altitude to GPS value.
-		alt = report.get('alt', 0.0)
-		print alt
-		camera.exif_tags['GPS.GPSAltitudeRef'] = '0' if alt > 0 else '1'
-		aalt = math.fabs(alt)
-		camera.exif_tags['GPS.GPSAltitude'] = '%d/100' % int(100 * aalt)
+		    # Set altitude to GPS value.
+		    alt = report.get('alt', 0.0)
+		    print alt
+		    camera.exif_tags['GPS.GPSAltitudeRef'] = '0' if alt > 0 else '1'
+		    aalt = math.fabs(alt)
+		    camera.exif_tags['GPS.GPSAltitude'] = '%d/100' % int(100 * aalt)
  
-		# Convert speed from m/s to km/h and set tag.
-		speed = report.get('speed', 0.0)
-		print speed
-		camera.exif_tags['GPS.GPSSpeedRef'] = 'K'
-		camera.exif_tags['GPS.GPSSpeed'] = '%d/1000' % int(3600 * speed)
+		    # Convert speed from m/s to km/h and set tag.
+		    speed = report.get('speed', 0.0)
+		    print speed
+		    camera.exif_tags['GPS.GPSSpeedRef'] = 'K'
+		    camera.exif_tags['GPS.GPSSpeed'] = '%d/1000' % int(3600 * speed)
  
-		# Set direction of motion and direction along which the picture is taken (assuming frontal view).
-		track = report.get('track', 0.0)
-		print track
-		camera.exif_tags['GPS.GPSTrackRef'] = 'T'
-		camera.exif_tags['GPS.GPSTrack'] = '%d/10' % int(10 * track)
-		camera.exif_tags['GPS.GPSImgDirectionRef'] = 'T'
-		camera.exif_tags['GPS.GPSImgDirection'] = '%d/10' % int(10 * track)
+		    # Set direction of motion and direction along which the picture is taken (assuming frontal view).
+		    track = report.get('track', 0.0)
+		    print track
+		    camera.exif_tags['GPS.GPSTrackRef'] = 'T'
+		    camera.exif_tags['GPS.GPSTrack'] = '%d/10' % int(10 * track)
+		    camera.exif_tags['GPS.GPSImgDirectionRef'] = 'T'
+		    camera.exif_tags['GPS.GPSImgDirection'] = '%d/10' % int(10 * track)
  
-		# Set GPS latitude.
-		lat = report.get('lat', 0.0)
-		print lat
-		camera.exif_tags['GPS.GPSLatitudeRef'] = 'N' if lat > 0 else 'S'
-		alat = math.fabs(lat)
-		dlat = int(alat)
-		mlat = int(60 * (alat - dlat))
-		slat = int(6000 * (60 * (alat - dlat) - mlat))
-		camera.exif_tags['GPS.GPSLatitude'] = '%d/1,%d/1,%d/100' % (dlat, mlat, slat)
+		    # Set GPS latitude.
+		    lat = report.get('lat', 0.0)
+		    print lat
+		    camera.exif_tags['GPS.GPSLatitudeRef'] = 'N' if lat > 0 else 'S'
+		    alat = math.fabs(lat)
+		    dlat = int(alat)
+		    mlat = int(60 * (alat - dlat))
+		    slat = int(6000 * (60 * (alat - dlat) - mlat))
+		    camera.exif_tags['GPS.GPSLatitude'] = '%d/1,%d/1,%d/100' % (dlat, mlat, slat)
  
-		# Set GPS longitude.
-		lon = report.get('lon', 0.0)
-		print lon
-		camera.exif_tags['GPS.GPSLongitudeRef'] = 'E' if lon > 0 else 'W'
-		alon = math.fabs(lon)
-		dlon = int(alon)
-		mlon = int(60 * (alon - dlon))
-		slon = int(6000 * (60 * (alon - dlon) - mlon))
-		camera.exif_tags['GPS.GPSLongitude'] = '%d/1,%d/1,%d/100' % (dlon, mlon, slon)
+		    # Set GPS longitude.
+		    lon = report.get('lon', 0.0)
+		    print lon
+		    camera.exif_tags['GPS.GPSLongitudeRef'] = 'E' if lon > 0 else 'W'
+		    alon = math.fabs(lon)
+		    dlon = int(alon)
+		    mlon = int(60 * (alon - dlon))
+		    slon = int(6000 * (60 * (alon - dlon) - mlon))
+		    camera.exif_tags['GPS.GPSLongitude'] = '%d/1,%d/1,%d/100' % (dlon, mlon, slon)
  
 			# Provide next image file name.
 			#yield '/media/data/pictures/' + now.strftime('%s') + '.jpg'
@@ -563,8 +563,8 @@ def takePicture():
 	t = threading.Thread(target=spinner)
 	#threads.append(t)
 	gpsd = gps.gps(mode=gps.WATCH_ENABLE)
-	#t = threading.Thread(target=wait)
-	#threads.append(t)
+	t = threading.Thread(target=wait)
+	threads.append(t)
 	t.start()
 
 	scaled = None
@@ -574,8 +574,8 @@ def takePicture():
 	  #global gpsd
 	# Connect to gpsd.
 	  #print "Inicia GPS"
-	  gpsp = GpsPoller() # create the thread
-	  gpsp.start() # start it up
+	  #gpsp = GpsPoller() # create the thread
+	  #gpsp.start() # start it up
 	  #gpsd = gps.gps(mode=gps.WATCH_ENABLE)
 	  #t = threading.Thread(target=wait)
 	  #j.start()
